8b5817e3a018084e23a5d08a296b0c52451753dc
Эдуард Вейдеман <veydemann@gmail.com>
2021-09-22 00:45:20 +0000

    Delete Main.java

--- Arithmetic/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    private static int c;
-
-    public static void main(String[] args) {
-        Arithmetic calculator = new Arithmetic(2, 3);
-        c = calculator.prod();
-        System.out.println(c);
-    }
-}
