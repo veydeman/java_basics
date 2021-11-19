5d29a4e54c150c90ac8a606dbd3e6c4fbb5c8492
Jenny Frost <veydemann@gmail.com>
2021-11-19 03:44:40 +0300

    FullNameFormatter, TotalEarningsCalculator modified

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -2,17 +2,45 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+        String input = scanner.nextLine();
+        //TODO:напишите ваш код тут, результат вывести в консоль.
+        //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        char a = ' ';
+        char b = 48;
+        char c = 57;
+        int space = input.indexOf(' ');
+        int space1 = input.lastIndexOf(' ');
+        int spaceCount = 0;
+        boolean isDigit = false;
+
+        if (input.isEmpty()) {
+            System.out.println("Введенная строка не является ФИО1");
+        }
+        if (!input.isEmpty()) {
+            for (int i = 0; i < input.length(); i++) {
+                if ((input.charAt(i) >= b) & (input.charAt(i) <= c)) {
+                    isDigit = true;
+                    break;
+                }
+                if (input.charAt(i) == a) {
+                    spaceCount++;
+                    if (spaceCount > 2)
+                        break;
+                }
+            }
+            if ((spaceCount != 2) | (isDigit)) {
+                System.out.println("Введенная строка не является ФИО");
+            } else if ((!isDigit) & (spaceCount == 2)) {
+                System.out.println("Фамилия: " + input.substring(0, space) + "\n" + "Имя: "
+                        + input.substring(space + 1, space1) + "\n" + "Отчество: " +
+                        input.substring(space1 + 1));
+            }
+        }
     }
-  }
-
 }
+
+
--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,19 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        //TODO: напишите ваш код, результат вывести в консоль
 
+        String substr = text.substring(15, 19);
+        String substr1 = text.substring(35, 39);
+        String substr2 = text.substring(56, 61);
+        int a = Integer.parseInt(substr);
+        int b = Integer.parseInt(substr1);
+        int c = Integer.parseInt(substr2);
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
 }
+
+
