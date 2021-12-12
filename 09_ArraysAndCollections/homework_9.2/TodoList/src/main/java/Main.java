abca623effa790d75f62249c6f134da85c03593f
Jenny Frost <veydemann@gmail.com>
2021-12-13 02:22:02 +0300

    Todolist modified

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 
 public class Main {
     private static TodoList todoList = new TodoList();
-
+    private static final String addRegex = "(add|ADD|Add)\\p{Space}\\p{Alpha}+";
+    private static final String addIndexRegex = "(add|ADD|Add)\\p{Space}\\p{Digit}+\\p{Space}\\p{Alpha}+";
+    private static final String listRegex = "list|LIST|List";
+    private static final String deleteRegex = "(delete|DELETE|Delete)\\p{Space}.*";
+    private static final String editRegex = "(edit|EDIT|Edit)\\p{Space}\\p{Digit}+\\p{Space}\\p{Alpha}+";
 
     public static void main(String[] args) {
         while (true) {
@@ -11,19 +15,19 @@ public class Main {
             String a = in.nextLine();
 
 
-            if (a.matches("(add|ADD|Add)\\p{Space}\\p{Alpha}+")) {
+            if (a.matches(addRegex)) {
                 todoList.add(a.substring(4));
             }
-            if (a.matches("(add|ADD|Add)\\p{Space}\\p{Digit}+\\p{Space}\\p{Alpha}+")) {
+            if (a.matches(addIndexRegex)) {
                 todoList.add(Integer.parseInt(a.substring(4, 5)), a.substring(6));
             }
-            if (a.matches("list|LIST|List")) {
+            if (a.matches(listRegex)) {
                 todoList.getTodos();
             }
-            if (a.matches("(delete|DELETE|Delete)\\p{Space}.*")) {
+            if (a.matches(deleteRegex)) {
                 todoList.delete(Integer.parseInt(a.substring(7)));
             }
-            if (a.matches("(edit|EDIT|Edit)\\p{Space}\\p{Digit}+\\p{Space}\\p{Alpha}+")) {
+            if (a.matches(editRegex)) {
                 todoList.edit(a.substring(7), Integer.parseInt(a.substring(5, 6)));
             }
         }
