f70903965497171c70e30add924a35a64dbc59aa
Jenny Frost <veydemann@gmail.com>
2021-12-13 14:24:17 +0300

    Todolist modified

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class Main {
     private static TodoList todoList = new TodoList();
-    private static final String addRegex = "(add|ADD|Add)\\p{Space}\\p{Alpha}+";
-    private static final String addIndexRegex = "(add|ADD|Add)\\p{Space}\\p{Digit}+\\p{Space}\\p{Alpha}+";
-    private static final String listRegex = "list|LIST|List";
-    private static final String deleteRegex = "(delete|DELETE|Delete)\\p{Space}.*";
-    private static final String editRegex = "(edit|EDIT|Edit)\\p{Space}\\p{Digit}+\\p{Space}\\p{Alpha}+";
+    private static final String ADD_REGEX = "(add|ADD|Add)\\p{Space}\\p{Alpha}+";
+    private static final String ADD_INDEX_REGEX = "(add|ADD|Add)\\p{Space}\\p{Digit}+\\p{Space}\\p{Alpha}+";
+    private static final String LIST_REGEX = "list|LIST|List";
+    private static final String DELETE_REGEX = "(delete|DELETE|Delete)\\p{Space}.*";
+    private static final String EDIT_REGEX = "(edit|EDIT|Edit)\\p{Space}\\p{Digit}+\\p{Space}\\p{Alpha}+";
 
     public static void main(String[] args) {
         while (true) {
@@ -15,19 +15,19 @@ public class Main {
             String a = in.nextLine();
 
 
-            if (a.matches(addRegex)) {
+            if (a.matches(ADD_REGEX)) {
                 todoList.add(a.substring(4));
             }
-            if (a.matches(addIndexRegex)) {
+            if (a.matches(ADD_INDEX_REGEX)) {
                 todoList.add(Integer.parseInt(a.substring(4, 5)), a.substring(6));
             }
-            if (a.matches(listRegex)) {
+            if (a.matches(LIST_REGEX)) {
                 todoList.getTodos();
             }
-            if (a.matches(deleteRegex)) {
+            if (a.matches(DELETE_REGEX)) {
                 todoList.delete(Integer.parseInt(a.substring(7)));
             }
-            if (a.matches(editRegex)) {
+            if (a.matches(EDIT_REGEX)) {
                 todoList.edit(a.substring(7), Integer.parseInt(a.substring(5, 6)));
             }
         }
