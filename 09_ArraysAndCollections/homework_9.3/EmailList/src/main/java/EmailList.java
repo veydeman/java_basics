d443a49a5638d8adb6aca6dd8d1cb151f2ffec5c
jenny <eveydemann@gmail.com>
2021-12-16 16:57:55 +0300

    EmailList, Main modified

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
 
 public class EmailList {
+    public static final String EMAIL_PATTERN = "(\\p{Alpha}+@\\p{Alpha}+)\\.(\\p{Alpha}+)";
+    TreeSet<String> treeSet = new TreeSet<>();
 
     public void add(String email) {
+        if (email.matches(EMAIL_PATTERN)) {
+            treeSet.add(email.toLowerCase());
+        }
         // TODO: валидный формат email добавляется
     }
 
-    public List<String> getSortedEmails() {
+    public ArrayList<String> getSortedEmails() {
+        ArrayList<String> a = new ArrayList<>();
+        for (String email : treeSet) {
+            System.out.println(email);
+            a.add(email);
+        }
         // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        return a;
     }
-
 }
--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/Main.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
+    private static EmailList emailList = new EmailList();
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
+    public static final String EMAIL_PATTERN = "(ADD|Add|add)\\p{Space}(\\p{Alpha}+@\\p{Alpha}+)\\.(\\p{Alpha}+)";
+    public static final String LIST_REGEX = "LIST|list|List";
+
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
         test@test.com
@@ -19,15 +22,21 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            
+
             //TODO: write code here
-            
+            if (input.matches(LIST_REGEX)) {
+                emailList.getSortedEmails();
+                continue;
+            }
+            if (input.matches(EMAIL_PATTERN)) {
+                emailList.add(input.substring(4));
+            } else System.out.println(WRONG_EMAIL_ANSWER);
         }
     }
 }
