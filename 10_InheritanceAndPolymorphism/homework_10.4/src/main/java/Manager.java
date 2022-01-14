a215bc23a94fdf167ff6ff8e24d39deee5e89c74
jenny <eveydemann@gmail.com>
2022-01-14 13:03:33 +0300

    Files deleted

--- 10_InheritanceAndPolymorphism/homework_10.4/src/main/java/Manager.java
+++
@@ -1,8 +0,0 @@
-public class Manager extends Employee {
-
-    public Manager() {
-        super();
-        moneyGot = (Math.random()*((250_000-50_000)+1))+250_000;
-        salary = super.salary + ((moneyGot * 5) / 100);
-    }
-}
--- 10_InheritanceAndPolymorphism/homework_10.4/src/main/java/TopManager.java
+++
@@ -1,8 +0,0 @@
-public class TopManager extends Employee {
-
-    public TopManager() {
-        super();
-        double baseSalary = super.salary;
-        salary = (baseSalary * 150) / 100;
-    }
-}
