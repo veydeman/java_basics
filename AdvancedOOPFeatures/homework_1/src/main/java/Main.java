991a7f2c4c4efa586a6e8ec44c4262d0d454a6a4
jenny <eveydemann@gmail.com>
2022-01-20 20:20:12 +0300

    Main class modified

--- AdvancedOOPFeatures/homework_1/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_1/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -6,10 +7,18 @@ public class Main {
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
-        System.out.println(staff);
+        sortBySalaryAndAlphabet(staff);
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        Collections.sort(staff, (o1, o2) -> {
+            if (o1.getSalary().equals(o2.getSalary()))
+                return o1.getName().compareTo(o2.getName());
+            else
+                return o1.getSalary().compareTo(o2.getSalary());
+        });
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
     }
 }
