bdb56cd989da40990325f2fbf5138061ab3de053
jenny <eveydemann@gmail.com>
2022-01-27 20:31:59 +0300

    Main class modified

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,10 +1,7 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -16,20 +13,18 @@ public class Main {
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
 
-        ArrayList<Terminal> terminals = airport.getTerminals().stream().collect(Collectors.
-                toCollection(ArrayList::new));
-        Date date = new Date();
-        final long HOUR = 3600 * 1000;
-        Date dateAfter = new Date(date.getTime() + 2 * HOUR);
-        ArrayList<Flight> result = new ArrayList<Flight>();
-        for (Terminal terminal : airport.getTerminals()){
-            terminal.getFlights().forEach(flight -> result.add(flight));
-        }
-        ArrayList<Flight> nearestFlights = new ArrayList<Flight>();
-        nearestFlights = result.stream().filter(flight -> flight.getDate().after(date) & flight.getDate()
-                .before(dateAfter) & flight.getType().equals(Flight.Type.DEPARTURE))
+        Calendar time = Calendar.getInstance();
+        time.setTime(new Date());
+        Calendar timeAfter = Calendar.getInstance();
+        timeAfter.setTime(new Date());
+        timeAfter.add(Calendar.HOUR_OF_DAY, 2);
+        ArrayList<Terminal> terminals = airport.getTerminals().stream().collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Flight> result = terminals.stream().flatMap(terminal -> terminal.getFlights().stream())
+                .collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Flight> nearestFlights = result.stream().filter(flight -> flight.getDate()
+                        .after(time.getTime()) & flight.getDate().before(timeAfter.getTime())
+                        & flight.getType().equals(Flight.Type.DEPARTURE))
                 .collect(Collectors.toCollection(ArrayList::new));
         return nearestFlights;
     }
-
 }
