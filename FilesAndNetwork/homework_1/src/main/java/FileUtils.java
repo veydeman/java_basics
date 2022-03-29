635ce65c4e74bf3f1ba2cf71deaddf1c0e679f7d
jenny <eveydemann@gmail.com>
2022-03-29 18:40:10 +0300

    FileUtils, Main modified

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,44 @@
+import java.io.File;
+
 public class FileUtils {
 
+    private static final int ONE = 1;
+    private static final int STEP = 1024;
+
     public static long calculateFolderSize(String path) {
-        return 0;
+        File folder = new File(path);
+        long sum = 0;
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        File[] files = folder.listFiles();
+        for (File f : files) {
+            sum += calculateFolderSize(f.getPath());
+        }
+        return sum;
+    }
+
+    public static String getHumanReadableSize(long size) {
+        long kb = size / STEP;
+        long mb = kb / STEP;
+        long gb = mb / STEP;
+        long tb = gb / STEP;
+
+        if (tb > ONE) {
+            return tb + " Tb";
+        }
+
+        if (gb > ONE) {
+            return gb + " Gb";
+        }
+
+        if (mb > ONE) {
+            return mb + " Mb";
+        }
+
+        if (kb > ONE) {
+            return kb + " Kb";
+        }
+        return Long.toString(size);
     }
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Введите путь до папки: ");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        System.out.println("Размер папки " + path + " составляет " +
+                FileUtils.getHumanReadableSize(FileUtils.calculateFolderSize(path)));
     }
 }
