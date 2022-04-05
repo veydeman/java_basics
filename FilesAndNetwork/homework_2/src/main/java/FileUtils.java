213cdad946db7007e98d3f2b77bea9ab9862da2c
jenny <eveydemann@gmail.com>
2022-04-05 18:28:49 +0300

    FileUtils, Main modified

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,6 +1,21 @@
-public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class FileUtils {
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+        File folder = new File(sourceDirectory);
+        File newFolder = new File(destinationDirectory);
+        if (folder.isDirectory()) {
+            File[] f = folder.listFiles();
+            for (File file1 : f) {
+                Files.copy(Path.of(file1.getAbsolutePath()), newFolder.toPath().resolve(file1.toPath().getFileName()));
+                if (file1.isDirectory()) {
+                    copyFolder(file1.getAbsolutePath(),
+                            newFolder.getAbsolutePath() + "\\" + file1.getName());
+                }
+            }
+        } else System.out.println("Укажите правильный путь к папке");
     }
 }
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите путь к папке: ");
-        Scanner scanner = new Scanner(System.in);
-        String path = scanner.nextLine();
-        System.out.println("Введите путь для сохранения папки: ");
-        Scanner scanner1 = new Scanner(System.in);
-        String copyPath = scanner.nextLine();
+        while (true) {
+            try {
+                System.out.println("Введите путь к папке: ");
+                Scanner scanner = new Scanner(System.in);
+                String path = scanner.nextLine();
+                System.out.println("Введите путь для копирования папки: ");
+                String copyPath = scanner.nextLine();
+                FileUtils.copyFolder(path, copyPath);
+            }catch (Exception exception){
+                exception.printStackTrace();
+            }
+        }
     }
 }
